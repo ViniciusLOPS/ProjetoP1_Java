67a96ad73c7e7610721f0cbb4a2b3aab6db2ad6f
ViniciusLOPS <viniciuslops@hotmail.com>
2018-04-17 12:32:04 -0300

    initial commit Add Mundo

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+import java.util.ArrayList;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Mundo m = new Mundo();
+        m.printMundo();
+        m.attMapa();
+        m.printMundo();
+    }
+}
