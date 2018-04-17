153e2580fa9b46bdbf691348202f3cbf180bc75f
ViniciusLOPS <viniciuslops@hotmail.com>
2018-04-17 15:24:39 -0300

    Add Moto that extends Veiculo

--- src/Main.java
+++ src/Main.java
@@ -8,9 +8,11 @@ public class Main {
         Veiculo v = new Veiculo(1, 5);
         ArrayList <Caminhao> caminhoes = new ArrayList<>();
         ArrayList <Carro> carros = new ArrayList<>();
+        ArrayList <Moto> motos = new ArrayList<>();
         for (int i = 0; i < 1; i++){
             caminhoes.add(new Caminhao());
             carros.add(new Carro());
+            motos.add(new Moto());
         }
         m.printMundo();
         m.attMapa();
@@ -20,5 +22,7 @@ public class Main {
                 " " +  caminhoes.get(0).getCor() + " " +  caminhoes.get(0).getSpeed() + " " + caminhoes.get(0).getCarga());
         System.out.println( " " + carros.get(0).getX() + " " + carros.get(0).getY() + " " +  carros.get(0).getFabrica() +
                 " " +  carros.get(0).getCor() + " " +  carros.get(0).getSpeed() + " " + carros.get(0).getPassageiros());
+        System.out.println( " " + motos.get(0).getX() + " " + motos.get(0).getY() + " " +  motos.get(0).getFabrica() +
+                " " +  motos.get(0).getCor() + " " +  motos.get(0).getSpeed() + " " + motos.get(0).getPassageiros());
     }
 }
---
+++ src/Moto.java
@@ -0,0 +1,15 @@
+import java.util.Random;
+
+public class Moto extends Veiculo {
+    private int passageiros;  ///< 1 a 2 passageiros
+    public Moto(){
+        super(3, 3);
+        setPassageiros();
+    }
+    private void setPassageiros(){
+        this.passageiros = gen.nextInt(2) + 1;
+    }
+    public int getPassageiros(){
+        return passageiros;
+    }
+}
